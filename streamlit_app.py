bfd339a99252c9133bff99c477930b938ab87623
jyeags45 <164065471+jyeags45@users.noreply.github.com>
2024-07-17 01:13:50 +0000

    added initial config and dashboard name

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
+
+st.set_page_config(
+    page_title="US Baby Name Dashboard",
+    page_icon="👶",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
 
 st.title('My first streamlit app')
 st.title('Second line to update')
