6266bcc2bcb5663623cdf5acc51003552d21d4d2
jyeags45 <164065471+jyeags45@users.noreply.github.com>
2024-07-17 02:12:49 +0000

    added initial sidebar list selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,14 @@ st.set_page_config(
 
 alt.themes.enable("dark")
 
-st.title('My first streamlit app')
-st.title('Second line to update')
-st.title('Third line to update')
-st.title('Fourth line to update.testTD')
+# loading in data frames
+
+df_top_baby_names_yr = pd.read_csv("Baby_Names_Start/Top_baby_names_by_state.csv")
+
+
+# creating side bar with year list
+with st.sidebar:
+    st.title("US Baby Names Dashboard")
+    
+    state_list = list(df_top_baby_names_yr.State.unique())
+    selected_state = st.selectbox("Select a state", state_list)
