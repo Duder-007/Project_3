d3f53eedf3675887a6df1c944e4b85c128cba894
jyeags45 <jackyeager45@gmail.com>
2024-07-16 18:34:32 -0600

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,18 @@ import pandas as pd
 
 st.title('My first streamlit app')
 st.title('Second line to update')
+
+# import streamlit as st
+# import pandas as pd
+# import matplotlib as pyplt
+# import altair as alt
+
+# # initial set up
+
+# st.set_page_config(
+#     page_title="US Baby Name Dashboard",
+#     page_icon="👶",
+#     layout="wide",
+#     initial_sidebar_state="expanded")
+
+# alt.themes.enable("dark")
