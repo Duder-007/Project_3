386c78afbb6ecaa7c5ddad57dcbc69f1d1acc47b
TAD <160682406+amirudial@users.noreply.github.com>
2024-07-17 00:40:45 +0000

    test TD

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,10 @@ import pandas as pd
 st.title('My first streamlit app')
 st.title('Second line to update')
 st.title('Third line to update')
+<<<<<<< HEAD
 st.title('fourth line to update')
 st.title('fifth line to update')
+=======
+st.title('Fourth line to update.testTD')
+
+>>>>>>> 11d8875 (test TD)
