cdd297ffb54793a99fadc681321a4808f631c63e
jyeags45 <jackyeager45@gmail.com>
2024-07-18 18:31:11 -0600

    swapped state selection for year selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,15 +12,15 @@ alt.themes.enable("dark")
 
 # loading in data frames
 
-df_top_baby_names_yr = pd.read_csv("Baby_Names_Start/Top_baby_names_by_state.csv")
+df_top_baby_names_yr = pd.read_csv("Baby_Names_Start/top_five_names_per_state.csv")
 
 
 # creating side bar with year list
 with st.sidebar:
     st.title("US Baby Names Dashboard")
     
-    state_list = list(df_top_baby_names_yr.State.unique())
-    selected_state = st.selectbox("Select a state", state_list)
+    year_list = list(df_top_baby_names_yr.Year.unique())
+    selected_year = st.selectbox("Select a year", year_list)
 
     # df_selected_state = [df_top_baby_names_yr == selected_state]
     # df_selected_state_sorted = df_selected_state.sort_values(by="Count")
