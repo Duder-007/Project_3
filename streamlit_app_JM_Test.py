56cbda7d56f1f39c5adb39b3027d014dbecadf71
duder-007 <159501857+Duder-007@users.noreply.github.com>
2024-07-18 18:28:33 -0700

    Update

--- streamlit_app_JM_Test.py
+++ streamlit_app_JM_Test.py
@@ -35,7 +35,23 @@ with st.sidebar:
             
             color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
             selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
-
+# Heatmap
+def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
+    heatmap = alt.Chart(input_df).mark_rect().encode(
+            y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+            x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+            color=alt.Color(f'max({input_color}):Q',
+                             legend=None,
+                             scale=alt.Scale(scheme=input_color_theme)),
+            stroke=alt.value('black'),
+            strokeWidth=alt.value(0.25),
+        ).properties(width=900
+        ).configure_axis(
+        labelFontSize=12,
+        titleFontSize=12
+        ) 
+    # height=300
+    return heatmap
 # Continue with the rest of the dashboard implementation
 # Example: Visualizing top baby names for the selected year and color theme
 # ...
